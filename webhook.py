a6cf1f8cd2bed6b021dc271fe3e35629fd87f9a1
braininfryingpan1 <47190780+braininfryingpan1@users.noreply.github.com>
2019-03-10 20:31:14 -0400

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
+    print("request",req)
     print(json.dumps(req, indent=4))
     
     res = processrequest(req)
